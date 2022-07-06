7ff4610ffd361bbf553f519697643d7f9aeec9aa
ANTHONY GATHOGO <106891513+antonioavanti@users.noreply.github.com>
2022-07-05 21:02:22 -0500

    loops

--- cupofjava.js
+++ cupofjava.js
@@ -423,3 +423,35 @@ switch (education){
 
 }
 console.log('In 2015, a person with '+education+ " earned an average of $"+salary.toLocaleString("en-US")+"/year")
+
+//LOOPS
+
+var start = 0 // when to start
+
+while (start < 10){ //when to stop
+		console.log(start);
+		start = start + 2; // how to get to the next item
+}
+
+//quiz 4-1: JuliaJames
+
+var x = 1
+
+while (x <= 20){
+
+	if (x % 3 === 0 && x % 5 === 0){
+		console.log("JuliaJames");
+
+	} else if ( x % 5 === 0){
+		console.log("James");
+
+
+	} else if (x % 3 === 0){
+		console.log("Julia");
+
+	} else{
+		console.log(x);
+	}
+
+	x = x + 1;
+}
