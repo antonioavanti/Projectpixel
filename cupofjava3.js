ab5fe371d74cb92a32cdc9e9b69b59be0b417610
ANTHONY GATHOGO <106891513+antonioavanti@users.noreply.github.com>
2022-07-10 17:28:24 -0500

    Arrays
    Halfway done with lesson 6.

--- cupofjava3.js
+++ cupofjava3.js
@@ -1,10 +1,29 @@
 console.log("Arrays start here!");
 
-var donut1 = "glazed";
-var donut2 = "chocolate frosted";
-var donut3 = "cinnamon";
-var donut4 = "sprinkled";
-var donut5 = "powdered";
-var donut6 = "cinnamon sugar";
-var donut7 = "glazed cruller";
-var donut8 = "chocolate cruller";
+var donuts = ["glazed","chocolate frosted","cinnamon","sprinkled","powdered","cinnamon sugar","glazed cruller","chocolate cruller"];
+
+console.log(donuts[4]);
+
+donuts[1] = "glazed cruller";
+console.log(donuts[1]);
+
+//quiz 6-1: UdaciFamily
+
+var udaciFamily = ["Julia", "James", "Anthony"];
+console.log(udaciFamily);
+
+//quiz 6-2: Building the crew
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+var crew = ["captain", "second", "pilot", "companion", "mercenary", "mechanic"];
+console.log(crew)
+
+var donuts = ["glazed", "powdered", "sprinkled"];
+console.log(donuts.length);
+
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
+donuts.splice(1, 1, "chocolate cruller", "creme de leche"); // removes "chocolate frosted" at index 1 and adds "chocolate cruller" and "creme de leche" starting at index 1
